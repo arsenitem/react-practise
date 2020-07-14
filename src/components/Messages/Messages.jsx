de2bc9c6f8241107879578b12a4f3e014877c4da
arsenitem <arsenitem@bk.ru>
2020-07-14 23:13:02 +0500

    dialogs implementation

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import './Messages.css';
+import {NavLink} from 'react-router-dom';
 
 function Messages(props) {
     return (
-        <div>
-            Dialogs
+        <div className="dialogs-main">
+            <div className="dialogs">
+                <div className="dialog-items">
+                    <div className="dialog active">
+                        <NavLink to="/messages/den">Den</NavLink>
+                    </div>
+                    <div className="dialog">
+                        <NavLink to="/messages/sano">Sano</NavLink>
+                    </div>
+                    <div className="dialog">
+                        <NavLink to="/messages/egor">Egor</NavLink>
+                    </div>
+                    <div className="dialog">
+                        <NavLink to="/messages/masha">Masha</NavLink>
+                    </div>
+                </div>
+            </div>
+            <div className="messages">
+                <div className="message">
+                    Ola
+                </div>
+                <div className="message">
+                    Ola bro
+                </div>
+                <div className="message">
+                    Hows ya doing?
+                </div>
+            </div>
         </div>
     );
 }
