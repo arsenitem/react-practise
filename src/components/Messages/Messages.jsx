2293a4c8cc6ffb9d8442b06d970a54d52eecbbae
arsenitem <arsenitem@bk.ru>
2020-07-15 14:18:02 +0500

    edit dialogs template

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -2,35 +2,59 @@ import React from 'react';
 import './Messages.css';
 import {NavLink} from 'react-router-dom';
 
+let dialogsData = [{id: "1", name: "Den"},{id: "2", name: "Sano"},{id: "3", name: "Egor"},{id: "4", name: "Mashs"}];
+
+let messagesData = [{message: "Ola", datetime:"25.06.2020 18:36", position:"left"},
+    {message: "Ola bro", datetime:"25.06.2020 18:36", position:"right"},
+    {message: "Hows ya doings", datetime:"25.06.2020 18:36", position:"left"}];
+
+function DialogItem(props) {
+    return (
+        <div>
+            <img src="https://pm1.narvii.com/6889/74979d4d2744ec6e27995b6e866f091d04c0b40cr1-515-414v2_uhq.jpg"></img>
+            <div className="dialog active">
+                <NavLink to={"/messages/" + props.id}>{props.name}</NavLink>
+            </div>
+        </div>
+       
+    );
+}
+
+function Message(props) {
+    return(
+        <div className="message">
+            <div class={"msg " + props.position}>
+                <div className="msg-text">
+                    {props.message}
+                </div>
+                <div className="msg-info">
+                    {props.datetime}
+                </div>
+            </div>
+        </div>
+    );
+}
 function Messages(props) {
+    let dialogElements =  dialogsData.map((item) => {
+        return <DialogItem id={item.id} name={item.name}/>
+    });
+
+    let messageElements = messagesData.map(item => <Message message={item.message} datetime={item.datetime} position ={item.position}/>)
+   
     return (
         <div className="dialogs-main">
             <div className="dialogs">
-                <div className="dialog-items">
-                    <div className="dialog active">
-                        <NavLink to="/messages/den">Den</NavLink>
-                    </div>
-                    <div className="dialog">
-                        <NavLink to="/messages/sano">Sano</NavLink>
-                    </div>
-                    <div className="dialog">
-                        <NavLink to="/messages/egor">Egor</NavLink>
-                    </div>
-                    <div className="dialog">
-                        <NavLink to="/messages/masha">Masha</NavLink>
-                    </div>
+                <div className="dialog-items">                
+                    {dialogElements}
                 </div>
             </div>
             <div className="messages">
-                <div className="message">
-                    Ola
+                <div class="msg-input">
+                   <textarea placeholder="new message"></textarea>
                 </div>
-                <div className="message">
-                    Ola bro
-                </div>
-                <div className="message">
-                    Hows ya doing?
-                </div>
+                <button className="btn right">Send</button>
+     
+                {messageElements}
             </div>
         </div>
     );
---
+++ src/components/Profile/ProfileInfo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ProfileInfo() {
+    return(
+        <div>
+            <h3>Arsenii T</h3>
+            Date of Birth: 19.09.1999 <br/>
+            City: Perm<br/>
+            Education: HSE`21<br/>
+            Interests: Snowboard
+        </div>
+    );
+}
+
+export default ProfileInfo;
